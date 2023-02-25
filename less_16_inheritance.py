d2e5cd302d426bdaef2e210e7b6a57f6db9d1bb1
sandemetrio <happy.milkman18@gmail.com>
2023-02-25 16:33:32 +0200

    Third task done

--- less_16_inheritance.py
+++ less_16_inheritance.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 """Task 1 School
 
 Make a class structure in python representing people at school. Make a base class called Person, a class called Student,
@@ -84,7 +85,7 @@ filter_leaps (takes a list of dates (integers) and removes those that are not 'l
 
 class Mathematician:
     def square_nums(self, list_: list):
-        return [i**2 for i in list_]
+        return [i ** 2 for i in list_]
 
     def remove_positives(self, list_: list):
         return [i for i in list_ if i <= 0]
@@ -93,22 +94,132 @@ class Mathematician:
         return [i for i in list_ if i % 4 == 0 and not i % 100 == 0 or i % 400 == 0]
 
 
+"""Task 3 Product Store
+
+Write a class Product that has three attributes:
+type, name, price
+Then create a class ProductStore, which will have some Products and will operate with all products in the store. All 
+methods, in case they can’t perform its action, should raise ValueError with appropriate error information.
+
+Tips: Use aggregation/composition concepts while implementing the ProductStore class. You can also implement additional
+
+ classes to operate on a certain type of product, etc.
+
+Also, the ProductStore class must have the following methods:
+
+add(product, amount) - adds a specified quantity of a single product with a predefined price premium for your 
+store(30 percent)
+set_discount(identifier, percent, identifier_type=’name’) - adds a discount for all products specified by input
+identifiers (type or name). The discount must be specified in percentage
+sell_product(product_name, amount) - removes a particular amount of products from the store if available, in other 
+case raises an error. It also increments income if the sell_product method succeeds.
+get_income() - returns amount of many earned by ProductStore instance.
+get_all_products() - returns information about all available products in the store.
+get_product_info(product_name) - returns a tuple with product name and amount of items in the store."""
+
+
+class Product:
+    def __init__(self, type_: str, name: str, price):
+        self.type = type_
+        self.name = name
+        self.price = price
+
+
+class ProductStore:
+    STORE = []
+    INCOME = 0
+
+    def __init__(self, store_name: str):
+        self.store_name = store_name
+        self.product = Product
+
+    def add(self, product: Product, amount: int):
+        for item in self.STORE:
+            if item['name'] == product.name:
+                item['amount'] += amount
+                print(f'{amount} {product.name} added, total amount: {item["amount"]}')
+                return True
+
+        prod = {
+            "type": product.type,
+            "name": product.name,
+            "price": round(product.price * 1.3, 2),
+            "amount": amount
+        }
+        self.STORE.append(prod)
+        print(f'New product {product.name} added, total amount: {amount}')
+
+    def set_discount(self, identifier, percent, identifier_type='name'):
+        for item in self.STORE:
+            if item[identifier_type] == identifier:
+                print(item['price'])
+                item['price'] = round(item['price'] * (100 - percent)/100, 2)
+                print(f'{percent}% discount set to {item["name"]}, now it"s only {item["price"]}')
+
+    def sell_products(self, product_name, amount):
+        for item in self.STORE:
+            if item['name'] == product_name:
+                if item['amount'] >= amount:
+                    item['amount'] -= amount
+                    self.INCOME = item['price'] * amount
+                    return True
+                else:
+                    print(f'Only {item["amount"]} {item["name"]} left')
+                    return False
+
+        print(f'No products such as {product_name}')
+        return False
+
+    def get_income(self):
+        return self.INCOME
+
+    def get_all_products(self):
+        pprint(self.STORE)
+
+    def get_product_info(self, identifier, identifier_type='name'):
+        for item in self.STORE:
+            if item[identifier_type] == identifier:
+                pprint(item)
+                return item
+        print(f'No products such as {identifier}')
+        return False
+
+
+
+
+
 
 
 def main():
-
     student_1 = Student('Ivan', 'Ivanenko', 19, 11, 'Football')
     teacher_1 = Teacher('Petro', 'Petrenko', 48)
 
     # print(student_1)
     # print(teacher_1)
-    m = Mathematician()
+    # m = Mathematician()
+    #
+    # assert m.square_nums([7, 11, 5, 4]) == [49, 121, 25, 16]
+    #
+    # assert m.remove_positives([26, -11, -8, 13, -90]) == [-11, -8, -90]
+    #
+    # assert m.filter_leaps([2001, 1884, 1995, 2003, 2020, 1700, 1800, 1900, 2100, 2200, 2300, 400]) == [1884, 2020, 400]
 
-    assert m.square_nums([7, 11, 5, 4]) == [49, 121, 25, 16]
+    silpo = ProductStore('Сільпо')
 
-    assert m.remove_positives([26, -11, -8, 13, -90]) == [-11, -8, -90]
+    # silpo.add(Product('Diary', 'Milk', 32.49), 20)
+    # silpo.add(Product('Bread', 'Lavash', 20.34), 10)
+    # silpo.add(Product('Bread', 'Toast', 15.59), 50)
+    # silpo.add(Product('Diary', 'Yogurt', 34.54), 20)
+    # silpo.add(Product('Diary', 'Ayran', 54.40), 5)
+    # silpo.add(Product('Diary', 'Ayran', 54.40), 5)
+    # silpo.set_discount('Lavash', 30)
 
-    assert m.filter_leaps([2001, 1884, 1995, 2003, 2020, 1700, 1800, 1900, 2100, 2200, 2300, 400]) == [1884, 2020, 400]
+    # silpo.sell_products('Toast', 10)
+    # silpo.sell_products('Toast', 10)
+
+    # print(silpo.get_income())
+    # silpo.get_all_products()
+    # silpo.get_product_info('Yogurt')
 
 
 
