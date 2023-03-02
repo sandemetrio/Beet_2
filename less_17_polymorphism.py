b389192f8d572616635359cfedc86096872554c0
sandemetrio <happy.milkman18@gmail.com>
2023-03-02 13:27:11 +0200

    1st done

---
+++ less_17_polymorphism.py
@@ -0,0 +1,27 @@
+"""Task 1 Method overloading.
+
+Create a base class named Animal with a method called talk and then create two subclasses: Dog and Cat, and make their
+own implementation of the method talk be different. For instance, Dog’s can be to print ‘woof woof’, while Cat’s can be
+to print ‘meow’. Also, create a simple generic function, which takes as input instance of a Cat or Dog classes and
+performs talk method on input parameter.  """
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def talk(self):
+        raise NotImplementedError('Must be implemented by subclass')
+
+
+class Dog(Animal):
+    def talk(self):
+        print('Bark')
+
+
+class Cat(Animal):
+    def talk(self):
+        print('Meow')
