26959e12735389b321edb3ecf355f9c68c89a438
sandemetrio <happy.milkman18@gmail.com>
2023-03-09 18:01:14 +0200

    2 variant of first task

--- less_19_iterators.py
+++ less_19_iterators.py
@@ -4,12 +4,21 @@ Create your own implementation of a built-in function enumerate, named `with_ind
 takes two parameters: `iterable` and `start`, default is 0. Tips: see the documentation for the
 enumerate function'''
 
+import itertools
 
-def with_index(iterable, start=10):
+
+def with_index_1(iterable, start=10):
     for element in iterable:
         yield start, element
         start += 1
 
 
-for i in with_index(range(5)):
-    print(i)
+def with_index_2(iterable, start=0):
+    return zip(itertools.count(start=start), iterable)
+#
+# for i in with_index_1(range(5)):
+#     print(i)
+
+
+# for i in with_index_2(range(5)):
+#     print(i)
