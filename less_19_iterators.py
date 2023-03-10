ca58374c54fa69ac4083f703c2f64e49e3aa6464
sandemetrio <happy.milkman18@gmail.com>
2023-03-10 15:07:15 +0200

    3d task

--- less_19_iterators.py
+++ less_19_iterators.py
@@ -15,6 +15,8 @@ def with_index_1(iterable, start=10):
 
 def with_index_2(iterable, start=0):
     return zip(itertools.count(start=start), iterable)
+
+
 #
 # for i in with_index_1(range(5)):
 #     print(i)
@@ -22,3 +24,64 @@ def with_index_2(iterable, start=0):
 
 # for i in with_index_2(range(5)):
 #     print(i)
+
+"""Task 2
+
+Create your own implementation of a built-in function range, named in_range(), which takes
+three parameters: `start`, `end`, and optional step. Tips: See the documentation for `range`
+function"""
+
+
+def in_range(start, end, step=1):
+    if start <= end and step >= 0:
+        while start < end:
+            yield start
+            start += step
+    elif start > end and step <= 0:
+        while start > end:
+            yield start
+            start += step
+    else:
+        return None
+
+
+"""Task 3
+
+Create your own implementation of an iterable, which could be used inside for-in loop. Also,
+add logic for retrieving elements using square brackets syntax."""
+
+
+class MyIternacci:
+    def __init__(self, num, start_1=0, start_2=1):
+        if num < 1:
+            raise ValueError("the length of the sequence must be greater than 0")
+        self.num = num
+        self.start_1 = start_1
+        self.start_2 = start_2
+        self.counter = 0
+        self.value = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.num == self.counter:
+            raise StopIteration
+        if self.num == 1:
+            return 0
+        elif self.num == 2:
+            return 1
+
+        self.counter += 1
+
+        self.value = self.start_1 + self.start_2
+        self.start_1 = self.start_2
+        self.start_2 = self.value
+        return self.value
+
+
+
+fib = MyIternacci(5)
+
+for i in fib:
+    print(i)
